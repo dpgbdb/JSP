01642f60fadf4989d1735d7f527a352f7c85552f
kimdm505 <rlaehdals606@gmail.com>
2022-11-22 19:47:17 +0900

    11 / 22 게시판실습

---
+++ Jboard2/src/main/java/kr/co/Jboard2/vo/TermsVO.java
@@ -0,0 +1,19 @@
+package kr.co.Jboard2.vo;
+
+public class TermsVO {
+	
+	private String Terms;
+	private String Privacy;
+	public String getTerms() {
+		return Terms;
+	}
+	public void setTerms(String terms) {
+		Terms = terms;
+	}
+	public String getPrivacy() {
+		return Privacy;
+	}
+	public void setPrivacy(String privacy) {
+		Privacy = privacy;
+	}
+}
